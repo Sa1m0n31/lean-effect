9d8790d654bf8d384c3fdfbf76905bdaa44ce255
Szymon Burak <sajmon0031@gmail.com>
2021-02-12 22:52:08 +0100

    First amendments

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const produkty4 = document.querySelector(".produktyRightItem:nth-child(4)>.produ
 const produkty5 = document.querySelector(".produktyRightItem:nth-child(5)>.produktyRightText");
 const produkty6 = document.querySelector(".produktyRightItem:nth-child(6)>.produktyRightText");
 const produkty7 = document.querySelector(".produktyRightItem:nth-child(7)>.produktyRightText");
+const produkty8 = document.querySelector(".produktyRightItem:nth-child(8)>.produktyRightText");
 
 const arrow1 = document.querySelector(".produktyRightItem:nth-child(1)>h3>.arrowWrapper>.arrow");
 const arrow2 = document.querySelector(".produktyRightItem:nth-child(2)>h3>.arrowWrapper>.arrow");
@@ -14,21 +15,32 @@ const arrow4 = document.querySelector(".produktyRightItem:nth-child(4)>h3>.arrow
 const arrow5 = document.querySelector(".produktyRightItem:nth-child(5)>h3>.arrowWrapper>.arrow");
 const arrow6 = document.querySelector(".produktyRightItem:nth-child(6)>h3>.arrowWrapper>.arrow");
 const arrow7 = document.querySelector(".produktyRightItem:nth-child(7)>h3>.arrowWrapper>.arrow");
+const arrow8 = document.querySelector(".produktyRightItem:nth-child(8)>h3>.arrowWrapper>.arrow");
 
-const products = [produkty1, produkty2, produkty3, produkty4, produkty5, produkty6, produkty7];
-const arrows = [arrow1, arrow2, arrow3, arrow4, arrow5, arrow6, arrow7];
+const products = [produkty1, produkty2, produkty3, produkty4, produkty5, produkty6, produkty7, produkty8];
+const arrows = [arrow1, arrow2, arrow3, arrow4, arrow5, arrow6, arrow7, arrow8];
+const productsOpen = [false, false, false, false, false, false, false, false];
 
 const produkty = async (n) => {
-    products.forEach(item => {
-        item.style.height = 0;
-        item.style.opacity = 0;
-    });
-    arrows.forEach(item => {
-        item.style.transform = "rotate(-90deg)";
-    });
-    products[n-1].style.height = "auto";
-    products[n-1].style.opacity = 1;
-    arrows[n-1].style.transform = "rotate(0)";
+    if(!productsOpen[n-1]) {
+        products.forEach(item => {
+            item.style.height = 0;
+            item.style.opacity = 0;
+        });
+        arrows.forEach(item => {
+            item.style.transform = "rotate(-90deg)";
+        });
+        products[n-1].style.height = "auto";
+        products[n-1].style.opacity = 1;
+        productsOpen[n-1] = true;
+        arrows[n-1].style.transform = "rotate(0)";
+    }
+    else {
+        products[n-1].style.height = 0;
+        products[n-1].style.opacity = 0;
+        productsOpen[n-1] = false;
+        arrows[n-1].style.transform = "rotate(-90deg)";
+    }
 }
 
 /* Hamburger menu */
@@ -37,7 +49,6 @@ const list = document.querySelector(".menuMobileList");
 const container = document.querySelector(".container");
 
 const openMenu = () => {
-    console.log("open");
     menu.style.width = "100%";
     menu.style.height = "100vh";
     list.style.opacity = 1;
@@ -46,7 +57,6 @@ const openMenu = () => {
 }
 
 const closeMenu = () => {
-    console.log("close");
     menu.style.height = "100vh";
     menu.style.width = "0";
     list.style.opacity = 0;
