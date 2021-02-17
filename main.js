0f936d5de4c5ed53980694566bb76e6faa78bd01
Szymon Burak <sajmon0031@gmail.com>
2021-02-17 02:35:52 +0100

    Amendments part 2

--- main.js
+++ main.js
@@ -8,6 +8,15 @@ const produkty6 = document.querySelector(".produktyRightItem:nth-child(6)>.produ
 const produkty7 = document.querySelector(".produktyRightItem:nth-child(7)>.produktyRightText");
 const produkty8 = document.querySelector(".produktyRightItem:nth-child(8)>.produktyRightText");
 
+const header1 = document.querySelector(".produktyRightItem:nth-child(1)>.produktyRightHeader");
+const header2 = document.querySelector(".produktyRightItem:nth-child(2)>.produktyRightHeader");
+const header3 = document.querySelector(".produktyRightItem:nth-child(3)>.produktyRightHeader");
+const header4 = document.querySelector(".produktyRightItem:nth-child(4)>.produktyRightHeader");
+const header5 = document.querySelector(".produktyRightItem:nth-child(5)>.produktyRightHeader");
+const header6 = document.querySelector(".produktyRightItem:nth-child(6)>.produktyRightHeader");
+const header7 = document.querySelector(".produktyRightItem:nth-child(7)>.produktyRightHeader");
+const header8 = document.querySelector(".produktyRightItem:nth-child(8)>.produktyRightHeader");
+
 const arrow1 = document.querySelector(".produktyRightItem:nth-child(1)>h3>.arrowWrapper>.arrow");
 const arrow2 = document.querySelector(".produktyRightItem:nth-child(2)>h3>.arrowWrapper>.arrow");
 const arrow3 = document.querySelector(".produktyRightItem:nth-child(3)>h3>.arrowWrapper>.arrow");
@@ -19,6 +28,7 @@ const arrow8 = document.querySelector(".produktyRightItem:nth-child(8)>h3>.arrow
 
 const products = [produkty1, produkty2, produkty3, produkty4, produkty5, produkty6, produkty7, produkty8];
 const arrows = [arrow1, arrow2, arrow3, arrow4, arrow5, arrow6, arrow7, arrow8];
+const headers = [header1, header2, header3, header4, header5, header6, header7, header8];
 const productsOpen = [false, false, false, false, false, false, false, false];
 
 const produkty = async (n) => {
@@ -27,6 +37,9 @@ const produkty = async (n) => {
             item.style.height = 0;
             item.style.opacity = 0;
         });
+        headers.forEach(item => {
+           item.style.color = "#2F281E";
+        });
         arrows.forEach(item => {
             item.style.transform = "rotate(-90deg)";
         });
@@ -34,8 +47,10 @@ const produkty = async (n) => {
         products[n-1].style.opacity = 1;
         productsOpen[n-1] = true;
         arrows[n-1].style.transform = "rotate(0)";
+        headers[n-1].style.color = "#FAB005";
     }
     else {
+        headers[n-1].style.color = "#2F281E";
         products[n-1].style.height = 0;
         products[n-1].style.opacity = 0;
         productsOpen[n-1] = false;
